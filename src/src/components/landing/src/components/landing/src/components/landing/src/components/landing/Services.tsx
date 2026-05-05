99772b2f041a90dc5da4feccc927ad34b9182d51
shravani28-18 <shravanim885@gmail.com>
2026-05-05 16:26:52 +0530

    Add Services component to display service offerings

---
+++ src/src/components/landing/src/components/landing/src/components/landing/src/components/landing/Services.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <section style={{ padding: "40px" }}>
+      <h2>Our Services</h2>
+      <ul>
+        <li>Website Development</li>
+        <li>Social Media Marketing</li>
+        <li>Branding</li>
+        <li>SEO</li>
+      </ul>
+    </section>
+  );
+};
